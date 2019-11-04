6b25c3c3e228eeec047e6fd99805359f18188dd4
sherrycayheyhey <sherrycaywood@yahoo.com>
2019-11-04 09:53:11 -0800

    Added checks to filter out repeat devices and results display in a ListView

--- app/src/main/java/com/chromsicle/bluetoothdemo/MainActivity.java
+++ app/src/main/java/com/chromsicle/bluetoothdemo/MainActivity.java
@@ -13,15 +13,22 @@ import android.content.IntentFilter;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     ListView listView;
     TextView statusTextView;
     Button searchButton;
+    ArrayList<String> bluetoothDevices = new ArrayList<>();
+    ArrayAdapter arrayAdapter;
+    //ArrayList used to filter out duplicate device with different dBm
+    ArrayList<String> addresses = new ArrayList<>();
 
     BluetoothAdapter bluetoothAdapter;
 
@@ -33,6 +40,10 @@ public class MainActivity extends AppCompatActivity {
         listView = findViewById(R.id.listView);
         statusTextView = findViewById(R.id.statusTextView);
         searchButton = findViewById(R.id.searchButton);
+
+        //set up the array
+        arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, bluetoothDevices);
+        listView.setAdapter(arrayAdapter);
 
         //allows us to work with this bluetooth adapter
         bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
@@ -61,6 +72,9 @@ public class MainActivity extends AppCompatActivity {
         statusTextView.setText("Searching...");
         //disable the button so the launch process isn't started again for a moment
         searchButton.setEnabled(false);
+        //clear the list whenever the button is pressed
+        bluetoothDevices.clear();
+        addresses.clear();
         //start searching for Bluetooth devices, don't forget to ask for permission in the manifest
         bluetoothAdapter.startDiscovery();
     }
@@ -86,10 +100,24 @@ public class MainActivity extends AppCompatActivity {
                 String address = device.getAddress();
                 //use rssi to get the device strength (nearer or further devices)
                 String rssi = Integer.toString(intent.getShortExtra(BluetoothDevice.EXTRA_RSSI, Short.MIN_VALUE));
-                Log.i("device found", "Name: " + name + " Address: " + address + "RSSI: " + rssi);
+                //Log.i("device found", "Name: " + name + " Address: " + address + "RSSI: " + rssi);
+                //add the address if it isn't already in the list
+                if(!addresses.contains(address)) {
+                    addresses.add(address);
+                    //use to check if the device has already been added to the list, we don't want repeats
+                    String deviceString = "";
+                    if (name == null || name.equals("")) {
+                        //the name is null or an empty String so use the address instead
+                        deviceString = address + " - RSSI" + rssi + "dBm";
+                    } else {
+                        deviceString = name + " - RSSI" + rssi + "dBm";
+                    }
+
+                    bluetoothDevices.add(deviceString);
+                    //update the adapter to let it know there's new info to display
+                    arrayAdapter.notifyDataSetChanged();
+                }
             }
-
-
         }
     };
 }
